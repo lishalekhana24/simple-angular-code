3722ab8e47f481980ef892332972c6255ad3e96f
lishalekhana24 <cs22b1024lisha@gmail.com>
2025-05-02 04:21:36 +0530

    Add files via upload

--- people-list.component.ts
+++ people-list.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PeopleService } from '../people.service';
 
 @Component({
   selector: 'app-people-list',
-  templateUrl: './people-list.component.html'
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent {
   people = [];
 
-  constructor(private peopleService: PeopleService) {
+  constructor(private peopleService: PeopleService, private router: Router) {
+    this.people = this.peopleService.getPeople();
+  }
+
+  editPerson(id: number) {
+    this.router.navigate(['/edit', id]);
+  }
+
+  deletePerson(id: number) {
+    this.peopleService.deletePerson(id);
     this.people = this.peopleService.getPeople();
   }
 }
